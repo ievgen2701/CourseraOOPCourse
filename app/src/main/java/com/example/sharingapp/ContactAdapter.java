f9b80bdd00d250be127a6c0b1aaca01bdaac1c70
ievgen2701 <ievgen.anikushyn@gamil.com>
2019-07-02 02:32:23 +0300

    implemented ContactAdapter

---
+++ app/src/main/java/com/example/sharingapp/ContactAdapter.java
@@ -0,0 +1,47 @@
+package com.example.sharingapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * ContactAdapter is responsible for what information is displayed in ListView entries.
+ */
+public final class ContactAdapter extends ArrayAdapter<Contact> {
+
+    private final LayoutInflater inflater;
+    private final Context context;
+
+    public ContactAdapter(final Context context, final ArrayList<Contact> contacts) {
+        super(context, 0, contacts);
+        this.context = context;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public View getView(final int position, View convertView, final ViewGroup parent) {
+        // getItem(position) gets the "contact" at "position" in the "contacts" ArrayList
+        // (where "contacts" is a parameter in the ContactAdapter creator as seen above ^^)
+        final Contact contact = getItem(position);
+        final String username = "Username: " + contact.getUsername();
+        final String email = "Email: " + contact.getEmail();
+        // Check if an existing view is being reused, otherwise inflate the view.
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.contactlist_contact,
+                    parent, false);
+        }
+        final TextView username_tv = convertView.findViewById(R.id.username_tv);
+        final TextView email_tv = convertView.findViewById(R.id.email_tv);
+        final ImageView photo = convertView.findViewById(R.id.contacts_image_view);
+        photo.setImageResource(android.R.drawable.ic_menu_gallery);
+        username_tv.setText(username);
+        email_tv.setText(email);
+        return convertView;
+    }
+}
